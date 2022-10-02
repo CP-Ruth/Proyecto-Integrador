a7b7e76e974bcf69cd6a70975c10a0fa2140c869
Ruth <elith.tati@gmail.com>
2022-10-02 18:25:17 -0300

    Primera parte

---
+++ Pagina/Pagina.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Pagina',
+  templateUrl: './Pagina.component.html',
+  styleUrls: ['./Pagina.component.css']
+})
+export class PaginaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
